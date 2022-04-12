1bef43c9f60ef1e28b5348d8406b598f5e9aa568
Alexanderghier <alexanderalexander630@gmail.com>
2022-04-12 11:36:09 -0700

    times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@
  *                      statting with 0.
  * @n: the value of the times table to be printed
  */
-void print_times_table(int n)
+void times_table(int n)
 {
 int num, mult, prod;
 
@@ -19,7 +19,7 @@ for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
-
+i
 prod = num * mult;
 
 if (prod <= 99)

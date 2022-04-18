6c0802fddc2f2d6c3fc1a593477230a02833d0a1
Alexanderghier <alexanderalexander630@gmail.com>
2022-04-18 09:51:41 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,14 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-
+#include <stdlib.h>
 /**
- * main - print password.
- *
- * Return: 0.
  */
-
 int main(void)
 {
 int num;
